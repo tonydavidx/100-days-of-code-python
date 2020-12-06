68a013ba6a8a759873f02dfc8b4090f76e36333d
Tony David <tonydavidx@gmail.com>
2020-12-06 13:29:19 +0530

    --amend

---
+++ day_1/day-1-project.py
@@ -0,0 +1,4 @@
+print('Welcome to the Band Name Generator')
+city = input("What's the name of the city you grew up in?\n")
+pet = input("What's yours pet's name?\n")
+print(f"\nyour Band Name could be {city} {pet}\n")
