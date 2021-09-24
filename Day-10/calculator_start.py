de398a7d5ce12e50a0cc3125c9df5fd68362e659
Tony David <tonydavidx@gmail.com>
2021-09-24 23:41:37 +0530

    day 10 completed

--- Day-10/calculator_start.py
+++ Day-10/calculator_start.py
@@ -1,18 +1,24 @@
+from art import logo
 
-def add(n1,n2):
+
+def add(n1, n2):
     return n1 + n2
 
-def substract(n1,n2):
+
+def substract(n1, n2):
     return n1 - n2
 
-def multiply(n1,n2):
+
+def multiply(n1, n2):
     return n1 * n2
 
-def divide(n1,n2):
+
+def divide(n1, n2):
     return n1 / n2
 
+
 operations = {
-    "+" : add,
+    "+": add,
     "-": substract,
     "*": multiply,
     "/": divide,
@@ -20,28 +26,29 @@ operations = {
 
 
 def calculator():
+    print(logo)
     isContinue = True
-    num1 = int(input('What is the first number? '))
+    num1 = float(input('What is the first number? '))
 
     for o in operations:
         print(o)
 
     while isContinue:
-        operation_symbol = input('Pick an operation: ')    
+        operation_symbol = input('Pick an operation: ')
 
-        num2 = int(input('What is the next number? '))
+        num2 = float(input('What is the next number? '))
 
         calculation = operations[operation_symbol]
 
-        answer = calculation(num1,num2)
+        answer = calculation(num1, num2)
 
         print(f"{num1} {operation_symbol} {num2} = {answer}")
 
-        if input(f"type 'y' to calculate with {answer} or 'n' to start new calculation: ")=='y':
+        if input(f"type 'y' to calculate with {answer} or 'n' to start new calculation: ") == 'y':
             num1 = answer
         else:
             isContinue = False
-            calculator()  
+            calculator()
 
 
 calculator()
