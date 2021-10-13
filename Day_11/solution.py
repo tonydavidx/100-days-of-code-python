87d3f6f68980a319ac51f550a5fd1ffd69237e45
Tony David <tonydavidx@gmail.com>
2021-10-13 23:53:27 +0530

    soulution hint 12 completed

--- Day_11/solution.py
+++ Day_11/solution.py
@@ -41,6 +41,17 @@ cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
 # cards = [10, 11, 10]
 
 
+def calculate_score(cards):
+
+    if sum(cards) == 21 and len(cards) == 2:
+        return 0
+    if 11 in cards and sum(cards) > 21:
+        cards.remove(11)
+        cards.append(1)
+
+    return sum(cards)
+
+
 def deal_card():
     cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
     card = random.choice(cards)
@@ -50,10 +61,28 @@ def deal_card():
 # Hint 5: Deal the user and computer 2 cards each using deal_card() and append().
 user_cards = []
 computer_cards = []
+game_over = False
 
 for _ in range(2):
     user_cards.append(deal_card())
     computer_cards.append(deal_card())
+while not game_over:
+    user_score = calculate_score(user_cards)
+    computer_score = calculate_score(computer_cards)
+    print(f"Your cards: {user_cards} current score: {user_score}")
+    print(f"Computer first card: {computer_cards[0]}")
+    if user_score == 0 or computer_score == 0 or user_score > 21:
+        game_over = True
+    else:
+        want_deal = input("Want to get another card 'Y' or 'N'").lower()
+        if want_deal == 'y':
+            user_cards.append(deal_card())
+        else:
+            game_over = True
+
+while computer_score != 0 and computer_score < 17:
+    computer_cards.append(deal_card)
+    calculate_score(computer_cards)
 
 # Hint 6: Create a function called calculate_score() that takes a List of cards as input
 # and returns the score.
