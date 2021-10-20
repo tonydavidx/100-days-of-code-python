eb5aec4a3df74e44d467cb75064941cd91aea12d
Tony David <tonydavidx@gmail.com>
2021-10-20 23:54:34 +0530

    day 13 complete

--- Day_13/1_bugs.py
+++ Day_13/1_bugs.py
@@ -19,32 +19,35 @@
 # print(dice_imgs[dice_num])
 # lists start from zero so we need to generate a number from 0 to 5
 
-# Play Computer
-year = int(input("What's your year of birth?"))
-if year > 1980 and year < 1994:
-    print("You are a millenial.")
-elif year >= 1994:
-    print("You are a Gen Z.")
+# # Play Computer
+# year = int(input("What's your year of birth?"))
+# if year > 1980 and year < 1994:
+#     print("You are a millenial.")
+# elif year >= 1994:
+#     print("You are a Gen Z.")
 
 # # Fix the Errors
 # age = input("How old are you?")
 # if age > 18:
 # print("You can drive at age {age}.")
 
-# #Print is Your Friend
+# Print is Your Friend
 # pages = 0
 # word_per_page = 0
 # pages = int(input("Number of pages: "))
-# word_per_page == int(input("Number of words per page: "))
+# print(f"pages {pages}")
+# word_per_page = int(input("Number of words per page: "))
+# print(f"word per page {word_per_page}")
 # total_words = pages * word_per_page
 # print(total_words)
 
-# #Use a Debugger
-# def mutate(a_list):
-#   b_list = []
-#   for item in a_list:
-#     new_item = item * 2
-#   b_list.append(new_item)
-#   print(b_list)
+# Use a Debugger
+def mutate(a_list):
+    b_list = []
+    for item in a_list:
+        new_item = item * 2
+        b_list.append(new_item)
+    print(b_list)
 
-# mutate([1,2,3,5,8,13])
+
+mutate([1, 2, 3, 5, 8, 13])
