8716ff3719d3b85ba2c391b6ddfecd1c3fd1a9c0
Tony David <tonydavidx@gmail.com>
2021-11-16 21:35:59 +0530

    oop coffee maker completed

--- Day_16/oop-coffee-machine/main.py
+++ Day_16/oop-coffee-machine/main.py
@@ -6,14 +6,22 @@ menu = Menu()
 coffee_maker = CoffeeMaker()
 money_machine = MoneyMachine()
 
+
 machine_on = True
+while machine_on:
+    choice = input(f'What would you like {menu.get_items()}\n')
 
-choice = input(f'What would you like {menu.get_items()}\n')
-drink =  menu.find_drink(choice)
+    if choice == 'report':
+        coffee_maker.report()
+        money_machine.report()
+    elif choice == 'off':
+        machine_on = False
 
-if coffee_maker.is_resource_sufficient(drink):
-    cost = drink.cost
-    print(f'{drink.name} cost is {cost}')
-    payment = money_machine.make_payment(cost)
-    if payment == True:
-        coffee_maker.make_coffee(drink) 
+    else:
+        drink = menu.find_drink(choice)
+        if coffee_maker.is_resource_sufficient(drink):
+            cost = drink.cost
+            print(f'{drink.name} cost is {cost}')
+            payment = money_machine.make_payment(cost)
+            if payment == True:
+                coffee_maker.make_coffee(drink)
